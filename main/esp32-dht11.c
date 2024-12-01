12d3d03e2c54961c9765083193f13d1db97119c8
nguyentito10 <tientoan91002003@gmail.com>
2024-12-01 22:11:10 +0700

    Update: Completed the explain of function dht11_read

--- main/esp32-dht11.c
+++ main/esp32-dht11.c
@@ -18,7 +18,7 @@ int wait_for_state(dht11_t dht11,int state,int timeout)
     
     while(gpio_get_level(dht11.dht11_pin) != state)                    
     {
-        if(count == timeout)                                            // If reached the timeout, return -1 (fail).
+        if(count == timeout)                                            // If reached the timeout, return -1.
             return -1;                                  
         count += 2;
         ets_delay_us(2);                                                // A short delay of 2 microseconds
@@ -47,12 +47,12 @@ void hold_low(dht11_t dht11, int hold_time_us)
 
 int dht11_read(dht11_t *dht11,int connection_timeout)
 {
-    int waited = 0;
-    int one_duration = 0;
-    int zero_duration = 0;
-    int timeout_counter = 0;
+    int waited = 0;                                                     // Tracks the duration for state changes.
+    int one_duration = 0;                                               // Measure the high pulse durations for decoding bits.
+    int zero_duration = 0;                                              // Measure the low pulse durations for decoding bits.
+    int timeout_counter = 0;                                            // Counts the number of retries.
 
-    uint8_t recieved_data[5] = 
+    uint8_t recieved_data[5] =                                          // Stores the raw data received from the sensor
     {
         0x00,
         0x00,
@@ -61,23 +61,26 @@ int dht11_read(dht11_t *dht11,int connection_timeout)
         0x00
     };
 
-    while(timeout_counter < connection_timeout)
+    while(timeout_counter < connection_timeout)                         
     {
         timeout_counter++;
         gpio_set_direction(dht11->dht11_pin,GPIO_MODE_INPUT);
-        hold_low(*dht11, 18000);
+        hold_low(*dht11, 18000);                                        // Hold_low 18us, start the signal
         
-        waited = wait_for_state(*dht11,0,40);
+                                                                        
+                                                                        // If any phase fails, the loop retries after a delay of 20 ms.
 
-        if(waited == -1)
+        waited = wait_for_state(*dht11,0,40);                           // Waits for the sensor to pull the line low.                         
+
+        if(waited == -1)    
         {
-            ESP_LOGE("DHT11:","Failed at phase 1");
+            ESP_LOGE("DHT11:","Failed at phase 1");     
             ets_delay_us(20000);
             continue;
         } 
 
 
-        waited = wait_for_state(*dht11,1,80);
+        waited = wait_for_state(*dht11,1,80);                           // Waits for the sensor to pull the line high.
         if(waited == -1) 
         {
             ESP_LOGE("DHT11:","Failed at phase 2");
@@ -85,7 +88,7 @@ int dht11_read(dht11_t *dht11,int connection_timeout)
             continue;
         } 
         
-        waited = wait_for_state(*dht11,0,80);
+        waited = wait_for_state(*dht11,0,80);                           // Waits for the sensor to pull the line low again.
         if(waited == -1) 
         {
             ESP_LOGE("DHT11:","Failed at phase 3");
@@ -96,18 +99,22 @@ int dht11_read(dht11_t *dht11,int connection_timeout)
         
     }
     
-    if(timeout_counter == connection_timeout) return -1;
+    if(timeout_counter == connection_timeout)                           // Return -1: timeout
+        return -1;
 
     for(int i = 0; i < 5; i++)
     {
         for(int j = 0; j < 8; j++)
         {
-            zero_duration = wait_for_state(*dht11,1,60);         
-            one_duration = wait_for_state(*dht11,0,80);
-            recieved_data[i] |= (one_duration > zero_duration) << (7 - j);
+            zero_duration = wait_for_state(*dht11,1,60);                        // Time spent at logic level 1
+            one_duration = wait_for_state(*dht11,0,80);                         // Time spent at logic level 0
+            recieved_data[i] |= (one_duration > zero_duration) << (7 - j);      // If one_duration > zero_duration, 
+                                                                                    // the bit is 1; 
+                                                                                    // otherwise, it’s 0
         }
     }
-    dht11->humidity = recieved_data[0] + recieved_data[1] /10.0 ;
+    dht11->humidity = recieved_data[0] + recieved_data[1] /10.0 ;               // recieved_data[0]: Phần đơn vị
+                                                                                // recieved_data[1] /10.0: Phần hàng chục
     dht11->temperature = recieved_data[2] + recieved_data[3] /10.0 ;
-    return 0;
+    return 0;                                                                   
 }
